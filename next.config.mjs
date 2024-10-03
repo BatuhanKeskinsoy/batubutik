4db7dc25ff2680e62a0444f25ce542f687918684
Batuhan Keskinsoy <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2024-10-03 22:45:19 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     remotePatterns: [
