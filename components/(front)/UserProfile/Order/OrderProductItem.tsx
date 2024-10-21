628d63052ea9d472ede3fe3d44a4016259ca5941
Batuhan Keskinsoy <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2024-10-21 13:30:31 +0300

    Modal Product Comment

---
+++ components/(front)/UserProfile/Order/OrderProductItem.tsx
@@ -0,0 +1,134 @@
+"use client";
+import ModalProductComment from "@/components/modals/ModalProductComment";
+import CustomButton from "@/components/others/CustomButton";
+import { siteURL } from "@/constants/(front)";
+import { getPrice } from "@/lib/functions/getPrice";
+import { orderProductTypes } from "@/types/order/orderTypes";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { IoChatbubbleEllipsesOutline, IoOpenOutline } from "react-icons/io5";
+
+interface IOrderProductItemProps {
+  item: orderProductTypes;
+}
+function OrderProductItem({ item }: IOrderProductItemProps) {
+  const [sendProductCommentModal, setSendProductCommentModal] = useState(false);
+
+  const handleSendProductCommentModal = async (e: any) => {
+    e.preventDefault();
+    setSendProductCommentModal(true);
+  };
+
+  const categorySlug = item.product.category_slug
+    ? `/${item.product.category_slug}`
+    : "";
+  const subCategorySlug = item.product.subCategory_slug
+    ? `/${item.product.subCategory_slug}`
+    : "";
+  const redirect = `${siteURL}/magaza/${item.product?.mainCategory_slug}${categorySlug}${subCategorySlug}/${item.product.slug}`;
+
+  return (
+    <>
+      {sendProductCommentModal && (
+        <ModalProductComment
+          slug={item.product.slug}
+          onClose={() => setSendProductCommentModal(false)}
+        />
+      )}
+      <div className="relative flex gap-4 items-center dark:bg-zinc-900 rounded-xl bg-white shadow-lg p-3">
+        {item.product.images && (
+          <Link
+            href={redirect}
+            title={`${item.product.brand || ""} ${item.product.title}`}
+            className={`relative rounded-lg shadow-md shadow-gray-400 dark:shadow-gray-800 overflow-hidden transition-all duration-300 lg:min-w-[100px] lg:w-[100px] lg:h-[156px] min-w-24 w-24 h-40`}
+          >
+            <Image
+              src={item.product.images[0].thumbnail}
+              fill
+              sizes="(max-width: 768px) 100%, 25%"
+              alt={`${item.product.brand || ""} ${item.product.title}`}
+              title={`${item.product.brand || ""} ${item.product.title}`}
+              className="object-cover hover:scale-105 transition-all duration-300"
+            />
+          </Link>
+        )}
+        <div
+          className={`flex flex-col justify-around h-full items-start w-full gap-2`}
+        >
+          <div className="flex flex-col gap-2 w-full h-full justify-around">
+            <div className="h-full flex flex-col gap-2 w-full justify-around">
+              <div className={`flex flex-col w-full gap-1`}>
+                <Link
+                  href={redirect}
+                  title={`${item.product.brand || ""} ${item.product.title}`}
+                  className={`font-medium line-clamp-1 transition-all duration-300 hover:text-site w-fit text-base`}
+                >
+                  <span className="font-bold tracking-wide text-site">
+                    {item.product.brand || ""}
+                  </span>{" "}
+                  {item.product.title}
+                </Link>
+                <span className={`text-gray-500 text-xs`}>
+                  {item.product.mainCategory}
+                  {item.product.category && ` / ${item.product.category}`}
+                  {item.product.subCategory && ` / ${item.product.subCategory}`}
+                </span>
+              </div>
+              <div
+                className={`flex flex-wrap gap-x-2 gap-y-1 line-clamp-2 max-w-full text-xs`}
+              >
+                {item.product.attributes &&
+                  item.attributes?.map((attr, key) => (
+                    <div
+                      key={key}
+                      className="flex after:content-[','] last:after:content-none max-w-full"
+                    >
+                      <div className="flex items-center gap-1 min-w-max">
+                        <span className="text-site">{attr?.attr_title} :</span>
+                        <span>{attr?.attr_options.option_name}</span>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+              <div className={`flex items-center gap-2 text-sm`}>
+                <span
+                  className={`font-medium ${
+                    item.product.discount > 0 ? "text-green-500 text-base" : ""
+                  }`}
+                >
+                  {getPrice(item.product.price)}
+                </span>
+                {item.product.discount > 0 && (
+                  <span className="line-through text-gray-500">
+                    {getPrice(
+                      (item.product.discount * item.product.price) / 100 +
+                        item.product.price
+                    )}{" "}
+                  </span>
+                )}
+              </div>
+            </div>
+
+            <CustomButton
+              leftIcon={<IoChatbubbleEllipsesOutline />}
+              title={"Ürünü Değerlendir"}
+              containerStyles="flex gap-2 items-center justify-center px-2 py-1.5 dark:bg-zinc-800 bg-gray-100 rounded-lg hover:bg-site dark:hover:bg-site hover:text-white transition-all duration-300 text-sm font-gemunu tracking-wider"
+              handleClick={(e) => handleSendProductCommentModal(e)}
+            />
+          </div>
+        </div>
+        <Link
+          href={redirect}
+          title={"Yeni Sekmede Aç"}
+          target="_blank"
+          className="absolute -top-3 -right-3 flex gap-2 items-center p-1.5 dark:bg-zinc-900 bg-white rounded-full hover:bg-site dark:hover:bg-site hover:text-white transition-all duration-300 border-4 dark:border-zinc-800 border-gray-100"
+        >
+          <IoOpenOutline className="text-sm" />
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default OrderProductItem;
---
+++ components/modals/ModalProductComment.tsx
@@ -0,0 +1,185 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import CustomButton from "@/components/others/CustomButton";
+import { IoCloseOutline } from "react-icons/io5";
+import { useProductDetail } from "@/hooks/useProduct";
+import Image from "next/image";
+import { getPrice } from "@/lib/functions/getPrice";
+import { getStar } from "@/lib/functions/getStar";
+
+interface ModalProps {
+  slug: string;
+  onClose: () => void;
+}
+
+const ModalProductComment: React.FC<ModalProps> = ({ slug, onClose }) => {
+  const [comment, setComment] = useState("");
+  const [rate, setRate] = useState(0);
+
+  useEffect(() => {
+    document.body.classList.add("noScroll");
+    return () => {
+      document.body.classList.remove("noScroll");
+    };
+  }, []);
+
+  const { productDetail, isLoading } = useProductDetail(slug);
+
+  return ReactDOM.createPortal(
+    <div className="fixed w-screen h-screen top-0 left-0 flex items-center justify-center z-20">
+      <div
+        className="bg-gray-900/80 w-screen h-screen fixed left-0 top-0 transition-all duration-300 animate-sidebarBgSmooth z-10"
+        onClick={onClose}
+      ></div>
+      {!isLoading ? (
+        <>
+          {productDetail && (
+            <div className="relative bg-white dark:bg-zinc-900 rounded-sm 2xl:w-[calc(100vw-55%)] xl:w-[calc(100vw-50%)] lg:w-[calc(100vw-40%)] lg:h-[calc(100dvh-54%)] w-[80%] h-[90%] shadow-lg shadow-gray-600 dark:shadow-gray-800 transition-all duration-300 animate-modalContentSmooth z-20">
+              <CustomButton
+                containerStyles="absolute lg:-right-4 lg:-top-4 -right-4 -top-4 text-gray-600 dark:text-gray-200 hover:bg-red-500 dark:hover:bg-red-500 hover:text-white transition-all duration-300 z-10 bg-white dark:bg-zinc-800 rounded-full"
+                leftIcon={<IoCloseOutline className="text-4xl" />}
+                handleClick={onClose}
+              />
+              <div className="flex flex-col w-full h-full gap-4">
+                <div className="relative flex w-full h-full">
+                  <div className="flex max-lg:flex-col gap-3 w-full h-full group">
+                    <div className="lg:w-1/2 w-full h-full">
+                      {productDetail.images && (
+                        <div className="relative w-full h-full">
+                          <Image
+                            src={productDetail.images[0].thumbnail}
+                            alt="add to cart product"
+                            fill
+                            sizes="(max-width: 768px) 50%, 20%"
+                            className="object-cover"
+                          />
+                        </div>
+                      )}
+                    </div>
+                    <div className="flex flex-col w-full gap-2.5 lg:p-6 p-4 pt-0 lg:!pl-2 h-full overflow-y-auto">
+                      <div className="flex justify-between items-center gap-2">
+                        <span className="text-gray-500 text-xs dark:text-gray-400 line-clamp-1">
+                          {productDetail.mainCategory}
+                          {productDetail.category &&
+                            ` / ${productDetail.category}`}
+                          {productDetail.subCategory &&
+                            ` / ${productDetail.subCategory}`}
+                        </span>
+                        <span className="text-gray-500 text-xs dark:text-gray-400 min-w-max">
+                          #{productDetail.code}
+                        </span>
+                      </div>
+                      {productDetail.rating && (
+                        <>
+                          {(() => {
+                            const size = 16;
+                            return (
+                              <div className="flex gap-2 items-center">
+                                <div className="flex gap-0.5 items-center min-w-max">
+                                  {Array.from({ length: 5 }).map((_, index) => (
+                                    <div
+                                      key={index}
+                                      className={`relative min-w-[${size}px] w-[${size}px] h-[${size}px}`}
+                                    >
+                                      {getStar(
+                                        index + 1,
+                                        productDetail.rating || 0,
+                                        size
+                                      )}
+                                    </div>
+                                  ))}
+                                </div>
+                                <p
+                                  className={`-mb-1`}
+                                  style={{ fontSize: size - 4 }}
+                                >
+                                  ({productDetail.rating})
+                                </p>
+                              </div>
+                            );
+                          })()}
+                        </>
+                      )}
+                      <div className="flex flex-col w-full gap-1">
+                        <span className="font-medium line-clamp-1 group-hover:text-site transition-all duration-300 lg:text-lg text-base">
+                          {productDetail.brand && (
+                            <>
+                              <span className="font-bold tracking-wide text-site">
+                                {productDetail.brand}
+                              </span>{" "}
+                            </>
+                          )}
+                          {productDetail.title}
+                        </span>
+                        <p className="line-clamp-2 text-gray-600 dark:text-gray-400 text-sm">
+                          {productDetail.short_content}
+                        </p>
+                      </div>
+                      <hr className="dark:border-zinc-800" />
+                      <div className="flex flex-col gap-2">
+                        <span className="font-gemunu text-lg tracking-wider">
+                          Yorum
+                        </span>
+                        <label
+                          htmlFor="comment"
+                          className="flex flex-col gap-4 w-full"
+                        >
+                          <textarea
+                            cols={6}
+                            rows={3}
+                            required
+                            className="bg-white dark:bg-zinc-900 border border-gray-200 dark:border-zinc-800 focus:border-site/50 dark:focus:border-site/50 rounded-lg py-3 px-4 outline-none w-full text-sm"
+                            placeholder="Yorumunuzu giriniz"
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                          />
+                        </label>
+                      </div>
+                      <div className="flex max-lg:flex-col justify-between items-center mt-2 gap-2">
+                        <div className="flex lg:flex-col max-lg:justify-between max-lg:w-full gap-2">
+                          <span className="font-gemunu text-lg tracking-wider">
+                            Değerlendirme
+                          </span>
+                          {(() => {
+                            const size = 20;
+                            return (
+                              <div className="flex items-center gap-2">
+                                <div className="flex gap-1 items-center min-w-max">
+                                  {Array.from({ length: 5 }).map((_, index) => (
+                                    <div
+                                      key={index}
+                                      className={`relative cursor-pointer min-w-[${size}px] w-[${size}px] h-[${size}px]`}
+                                      onClick={() => setRate(index + 1)}
+                                    >
+                                      {getStar(index + 1, rate || 0, size)}
+                                    </div>
+                                  ))}
+                                </div>
+                              </div>
+                            );
+                          })()}
+                        </div>
+                        <CustomButton
+                          title="Değerlendirmeyi Onayla"
+                          containerStyles="mt-1.5 flex lg:justify-end justify-center font-gemunu tracking-wider text-lg dark:bg-zinc-800 bg-gray-200 hover:bg-site dark:hover:bg-site hover:text-white w-full lg:w-fit py-2 px-4 rounded-md transition-all duration-300"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className="animate-modalContentSmooth z-10">
+          <div className="animate-spin rounded-full m-0.5 lg:size-24 size-16 border-t-4 border-b-4 border-gray-200 group-hover:border-white"></div>
+        </div>
+      )}
+    </div>,
+    document.body
+  );
+};
+
+export default ModalProductComment;
